796f7cc00d309e969998f1768df9fb9b7baa6f7c
yixin <yixinl3@uw.edu>
2023-05-09 03:32:37 -0700

    HW7

---
+++ attack.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+mongoClient = MongoClient("mongodb://localhost/pokemon")
+pokemonDB = mongoClient['pokemondb']
+pokemonColl = pokemonDB['pokemon_data']
+
+separator = ''.join(['-' for _ in range(50)]) 
+print(separator)
+
+query = {"attack": {"$gt": 150}}
+results_pokemon = pokemonColl.find(query)
+
+for pokemon in results_pokemon:
+    print(pokemon)
